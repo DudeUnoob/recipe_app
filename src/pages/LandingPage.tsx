2c79bbeb4f56fcc6b584928ff8856e8780d06bf9
Damodar Kamani <44341783+DudeUnoob@users.noreply.github.com>
2024-09-06 23:15:52 -0500

    Fixed erros in production

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+//import React from 'react';
 import  { Button }  from '../components/ui/button';
 import FeatureCard  from '../components/FeatureCard';
 import  TestimonialCard from '../components/TestimonialCard';
