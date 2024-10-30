c3947d2e7b587e927883fc8658b848ab7b4cc0b9
Damodar Kamani <44341783+DudeUnoob@users.noreply.github.com>
2024-10-29 23:31:45 -0500

    Change env variable name

--- server/src/functions/groq.ts
+++ server/src/functions/groq.ts
@@ -1,9 +1,10 @@
 import Groq from 'groq-sdk';
 import dotenv from 'dotenv';
 const envPath = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development';
-dotenv.config({ path: envPath });
+
+dotenv.config({ path: ".env" });
 
 export const client = new Groq({
-    apiKey: process.env.AI_KEY
+    apiKey: process.env.GROQ_API_KEY
 })
 
